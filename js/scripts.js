18d83a8e91baec163a169a479caefeee160befba
ryuunosuuke <99328850+ryuunosuuke@users.noreply.github.com>
2022-09-29 14:11:26 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Custom Scripts
+// Custom scripts
+
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 80,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
